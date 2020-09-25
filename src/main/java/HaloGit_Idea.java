4e50128849b31d252257c4faeb409c245052d816
gujii <117010@gujii.com>
2020-09-25 19:44:49 +0800

    idea 分支 6 commit

--- src/main/java/HaloGit_Idea.java
+++ src/main/java/HaloGit_Idea.java
@@ -3,5 +3,6 @@ public class HaloGit_Idea {
         System.out.println("Idea: Halo_Git!");
         System.out.println("Idea2: Halo_Git!");
         System.out.println("Idea3: Halo_Git!");
+        System.out.println("Idea4分支: Halo_Git!");
     }
 }
