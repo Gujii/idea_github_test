20e8a965504a672529e92273ed53f30a7d7b481b
gujii <117010@gujii.com>
2020-09-25 19:41:50 +0800

    idea 5 commit

--- src/main/java/HaloGit_Idea.java
+++ src/main/java/HaloGit_Idea.java
@@ -2,5 +2,6 @@ public class HaloGit_Idea {
     public static void main(String[] args) {
         System.out.println("Idea: Halo_Git!");
         System.out.println("Idea2: Halo_Git!");
+        System.out.println("Idea3: Halo_Git!");
     }
 }
